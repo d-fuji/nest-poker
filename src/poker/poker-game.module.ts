9c07557c394677601a8913205765ff5d219bfdc4
dfujii <dfujii@netprotections.co.jp>
2022-08-25 22:30:41 +0900

    fix: providersを修正

--- src/poker/poker-game.module.ts
+++ src/poker/poker-game.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { PokerGameController } from './poker-game.controller'
+import { DistributeCardsService } from './service/distribute-cards.service'
 import { JudgePokerWinnerService } from './service/judge-poker-winner.service'
 
 @Module({
   controllers: [PokerGameController],
-  providers: [JudgePokerWinnerService],
+  providers: [JudgePokerWinnerService, DistributeCardsService],
 })
 export class PokerGameModule {}
